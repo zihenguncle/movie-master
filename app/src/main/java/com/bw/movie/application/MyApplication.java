2b2cefa82912e84295397494250648f276e359b5
恒恒 <1263543288@qq.com>
2019-01-23 19:18:38 +0800

    沉浸式的更新

--- app/src/main/java/com/bw/movie/application/MyApplication.java
+++ app/src/main/java/com/bw/movie/application/MyApplication.java
@@ -8,25 +8,13 @@ import android.view.WindowManager;
 
 public class MyApplication extends Application {
     private static Context context;
-    //绘制页面时参照的设计图宽度
-    public final static float DESIGN_WIDTH = 750;
     @Override
     public void onCreate() {
         super.onCreate();
         context=getApplicationContext();
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        resetDensity();
-    }
 
-    private void resetDensity() {
-        Point size = new Point();
-        ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
-        getResources().getDisplayMetrics().xdpi = size.x/DESIGN_WIDTH*72f;
-    }
 
     public static Context MyApplication() {
         return context;
--- app/src/main/java/com/bw/movie/base/BaseActivity.java
+++ app/src/main/java/com/bw/movie/base/BaseActivity.java
@@ -8,8 +8,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(getViewById());
         initData();
     }
+
+    protected abstract int getViewById();
 
     protected abstract void initData();
 }
