5dfd2c11975885afa2cd588ed5deaaaff65c4eb8
Yangjun <2602863729@qq.com>
2019-01-23 20:55:23 +0800

    测试

--- app/src/main/java/com/bw/movie/login_success/home_fragment/HomeFragment.java
+++ app/src/main/java/com/bw/movie/login_success/home_fragment/HomeFragment.java
@@ -2,12 +2,13 @@ package com.bw.movie.login_success.home_fragment;
 
 import android.view.View;
 
+import com.bw.movie.R;
 import com.bw.movie.base.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
     @Override
     protected int getViewById() {
-        return 0;
+        return R.layout.fargment_home;
     }
 
     @Override
--- app/src/main/java/com/bw/movie/login_success/nearby_cinema_fragment/NearbyCinemaFragment.java
+++ app/src/main/java/com/bw/movie/login_success/nearby_cinema_fragment/NearbyCinemaFragment.java
@@ -2,12 +2,13 @@ package com.bw.movie.login_success.nearby_cinema_fragment;
 
 import android.view.View;
 
+import com.bw.movie.R;
 import com.bw.movie.base.BaseFragment;
 
 public class NearbyCinemaFragment extends BaseFragment {
     @Override
     protected int getViewById() {
-        return 0;
+        return R.layout.fargment_nearbycinema;
     }
 
     @Override
