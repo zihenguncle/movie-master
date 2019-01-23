ce46b9287e1683de0b1ed6a7908ee4c05bff699f
SunnyLovey <1669579025@qq.com>
2019-01-23 19:04:35 +0800

    测试包

--- app/src/main/java/com/bw/movie/application/MyApplication.java
+++ app/src/main/java/com/bw/movie/application/MyApplication.java
@@ -2,15 +2,32 @@ package com.bw.movie.application;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.view.WindowManager;
 
 public class MyApplication extends Application {
     private static Context context;
+    //绘制页面时参照的设计图宽度
+    public final static float DESIGN_WIDTH = 750;
     @Override
     public void onCreate() {
         super.onCreate();
         context=getApplicationContext();
-
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        resetDensity();
+    }
+
+    private void resetDensity() {
+        Point size = new Point();
+        ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
+        getResources().getDisplayMetrics().xdpi = size.x/DESIGN_WIDTH*72f;
+    }
+
     public static Context MyApplication() {
         return context;
     }
