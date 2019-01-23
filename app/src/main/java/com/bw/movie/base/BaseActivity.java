37f2495444e5f29efd17b2067c3823f2c01555f7
恒恒 <1263543288@qq.com>
2019-01-23 19:10:03 +0800

    抽基类

---
+++ app/src/main/java/com/bw/movie/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.bw.movie.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initData();
+    }
+
+    protected abstract void initData();
+}
---
+++ app/src/main/java/com/bw/movie/base/BaseFragment.java
@@ -0,0 +1,28 @@
+package com.bw.movie.base;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getViewById(),container,false);
+    }
+
+    protected abstract int getViewById();
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData(view);
+    }
+
+    protected abstract void initData(View view);
+}
