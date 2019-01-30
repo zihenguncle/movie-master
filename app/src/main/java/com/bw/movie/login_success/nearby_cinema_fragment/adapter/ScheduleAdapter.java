3baba7be77f766b3baf0bf65b66a03430dccdb59
SunnyLovey <1669579025@qq.com>
2019-01-30 11:19:33 +0800

    Merge branch 'master' of https://github.com/zihenguncle/movie-master
    # Conflicts:
    #	app/src/main/java/com/bw/movie/mvp/utils/Apis.java

--- app/src/main/java/com/bw/movie/login_success/nearby_cinema_fragment/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/bw/movie/login_success/nearby_cinema_fragment/adapter/ScheduleAdapter.java
@@ -42,10 +42,21 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        viewHolder.textView_screeningHall.setText(list.get(i).getScreeningHall());
-        viewHolder.textView_beginTime.setText(list.get(i).getBeginTime());
-        viewHolder.textView_text_endTime.setText(list.get(i).getEndTime()+"  end");
-        viewHolder.textView_fare.setText(list.get(i).getPrice()+"");
+
+        if(list.get(i).getStatus()==2){
+            viewHolder.textView_screeningHall.setText("该电影排期已过期");
+            viewHolder.textView_beginTime.setText(list.get(i).getBeginTime());
+            viewHolder.textView_text_endTime.setText(list.get(i).getEndTime()+"  end");
+            viewHolder.textView_fare.setText(list.get(i).getPrice()+"");
+            viewHolder.imageView_next.setVisibility(View.INVISIBLE);
+
+        }else {
+            viewHolder.textView_screeningHall.setText(list.get(i).getScreeningHall());
+            viewHolder.textView_beginTime.setText(list.get(i).getBeginTime());
+            viewHolder.textView_text_endTime.setText(list.get(i).getEndTime()+"  end");
+            viewHolder.textView_fare.setText(list.get(i).getPrice()+"");
+            viewHolder.imageView_next.setVisibility(View.VISIBLE);
+        }
 
 
     }
